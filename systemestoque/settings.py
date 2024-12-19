2c496f15fedab61f2ecfaf0cefc14f59e9e9e1a8
Arthur Reis <110342375+arthuzao22@users.noreply.github.com>
2024-12-19 14:05:30 -0300

    Feat: Alguns ajustes do layout da pagina

--- systemestoque/settings.py
+++ systemestoque/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-essns=lr^jj^!!2w$&l12h5n*ec=!=3^o#^2h(ko^xdqwrug*b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.2.127', '*']
 
 
 # Application definition
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'systemestoque.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'postgres',  # Nome do banco de dados
-#        'USER': 'postgres',  # Usuário
-#        'PASSWORD': '0iCvDs3KpSqQiOLU',  # Senha
-#        'HOST': 'imprudently-savvy-zander.data-1.use1.tembo.io',  # Hostname do servidor
-#        'PORT': '5432',  # Porta do banco de dados
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',  # Nome do banco de dados
+        'USER': 'postgres',  # Usuário
+        'PASSWORD': '0iCvDs3KpSqQiOLU',  # Senha
+        'HOST': 'imprudently-savvy-zander.data-1.use1.tembo.io',  # Hostname do servidor
+        'PORT': '5432',  # Porta do banco de dados
     }
 }
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 
 
